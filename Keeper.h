b44fd69e553b7846ac449a4cfc274ea03de1508d
kunaychik <101063646+kunaychik@users.noreply.github.com>
2022-10-16 20:12:22 +0300

    Update Keeper.h

--- Keeper.h
+++ Keeper.h
@@ -7,7 +7,24 @@
 #include "bus.h"
 
 class Keeper {
+public:
 
+    Keeper();
+    ~Keeper();
+
+    void add_new_vehicle(); //
+    void delete_vehicle(); //
+    void edit(); //+
+    void show(); // +
+    void save(); //
+    void upload_from_file(); //
+
+private:
+    Garage** my_garage;
+    uint16_t count_of_vehicle;
+
+    Garage* create_vehicle();
+    Garage** add_vehicle_to_garage(Garage*);
 };
 
 
