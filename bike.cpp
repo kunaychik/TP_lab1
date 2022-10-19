c54f3210fccf8d87207e6d74ef56411dd014930b
kunaychik <101063646+kunaychik@users.noreply.github.com>
2022-10-19 22:16:20 +0300

    Update bike.cpp

--- bike.cpp
+++ bike.cpp
@@ -1,25 +1,25 @@
 #include "bike.h"
 
 Bike::Bike() {
-    std::cout << "Enter brand: ";
+    std::cout << "Enter brand: " << std::endl;
     std::cin >> brand;
 
-    std::cout << "Enter model: ";
+    std::cout << "Enter model: " << std::endl;
     std::cin >> model;
 
-    std::cout << "Enter engine capacity: ";
+    std::cout << "Enter engine capacity: " << std::endl;
     std::cin >> engine_capacity;
     InvalidInput;
     if(engine_capacity <= 0) throw "There cannot be less than 0 engine capacity";
 
 
-    std::cout << "Enter engine power: ";
+    std::cout << "Enter engine power: " << std::endl;
     std::cin >> engine_power;
     InvalidInput;
     if(engine_power <= 0) throw "There cannot be less than 0 engine power";
 
 
-    std::cout << "Enter type of terrain: ";
+    std::cout << "Enter type of terrain: " << std::endl;
     std::cin >> terrain_type;
 }
 
@@ -42,7 +42,7 @@ Bike::Bike(const Bike &bike) {
 void Bike::show() {
     std::cout << "Bus." << std::endl;
     std::cout << "Brand: " << brand << std::endl;
-    std::cout << "Model:" << model << std::endl;
+    std::cout << "Model: " << model << std::endl;
     std::cout << "Engine capacity: " << engine_capacity << std::endl;
     std::cout << "Engine power: " << engine_power << std::endl;
     std::cout << "Type of terrain: " << terrain_type << std::endl;
@@ -62,35 +62,39 @@ void Bike::edit() {
 
     switch (choice) {
         case 1:
+            std::cout << "New Brand" << std::endl;
             std::cin >> brand;
             break;
         case 2:
+            std::cout << "New Model" << std::endl;
             std::cin >> model;
             break;
         case 3:
+            std::cout << "New Engine capacity" << std::endl;
             std::cin >> engine_capacity;
             InvalidInput;
             if(engine_capacity <= 0) throw "There cannot be less than 0 engine capacity";
             break;
         case 4:
+            std::cout << "New Engine power" << std::endl;
             std::cin >> engine_power;
             InvalidInput;
             if(engine_power <= 0) throw "There cannot be less than 0 engine power";
             break;
         case 5:
+            std::cout << "New Type of terrain" << std::endl;
             std::cin >> terrain_type;
             break;
         default:
             throw "Invalid value";
-            break;
     }
 }
 
 void Bike::save_to_file(std::ofstream &out) {
-    out << BIKE;
-    out << brand;
-    out << model;
-    out << engine_capacity;
-    out << engine_power;
-    out << terrain_type;
+    out << BIKE << std::endl;
+    out << brand << std::endl;
+    out << model << std::endl;
+    out << engine_capacity << std::endl;
+    out << engine_power << std::endl;
+    out << terrain_type << std::endl;
 }
